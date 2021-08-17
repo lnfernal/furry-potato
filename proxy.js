e8ecbcdd4139cd15a35e5af31012e5fd311ddc75
skye5000 <skylykat69@gmail.com>
2021-08-17 14:10:36 +0300

    add simple proxy

---
+++ proxy.js
@@ -0,0 +1,28 @@
+const config = require("./config.js");
+const net = require("net");
+
+const server = net.createServer((c) => {
+    console.log("Client Connected.");
+    let pc = net.createConnection({ host: config.host, port: config.port }, () => {
+        console.log("ProxyClient Connected.");
+        pc.on("data", (data) => {
+            c.write(data);
+            process.stdout.write("\u001b[31;1m");
+            console.log(data);
+        });
+        pc.on("end", () => {
+            console.log("ProxyClient Disconnected.");
+        });
+    });
+    c.on("data", (data) => {
+        pc.write(data);
+        process.stdout.write("\u001b[32;1m");
+        console.log(data);
+    });
+    c.on("end", () => {
+        console.log("Client Disconnected.");
+        pc.end();
+    });
+});
+
+server.listen(7777);
